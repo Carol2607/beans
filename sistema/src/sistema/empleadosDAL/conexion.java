f6128252044b2504827195082d364927ad3de5eb
Carolina Lopez <>
2022-09-13 20:15:38 -0500

    mi primer commit

---
+++ sistema/src/sistema/empleadosDAL/conexion.java
@@ -0,0 +1,48 @@
+
+package sistema.empleadosDAL;
+import java.sql.*;
+public class conexion {
+    
+    String strConexionDB ="jdbc:sqlite:C:/Users/Admin/Desktop/proyectoJDBC/sistema.db";
+    
+    Connection conn = null;
+    
+    public conexion (){
+    
+        try{
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection(strConexionDB);
+            System.out.println("Conexion Establecida");
+        }
+        catch(Exception e) {
+            System.out.println("Error en la conexion "+ e);
+        }
+        
+    }
+    public int ejecutarSentenciaSQL(String strSentenciaSQL){
+        try {
+            PreparedStatement pstm = conn.prepareStatement(strSentenciaSQL);
+            pstm.execute();
+            return 1;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return 0;
+        }
+    }    
+    public ResultSet consultarRegistros(String strSentenciaSQL){
+    
+        try{
+            PreparedStatement pstm = conn.prepareStatement(strSentenciaSQL);
+            ResultSet respuesta = pstm.executeQuery();
+            return respuesta;
+        }
+    
+        catch(Exception e){
+            System.out.println(e);
+            return null;  
+        }
+    }
+
+    
+}
